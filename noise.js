464e26304213e15259ce33f36c3e856614fee7dc
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-20 15:57:27 -0800

    Added .load which lets you repeatedly play a note & notes simultaneously. Removed console.log

--- noise.js
+++ noise.js
@@ -4,19 +4,18 @@ $(document).ready(() => {
 
   const playNote = function playNote(note) {
     const noteAudio = $(`#${note.toLowerCase()}Audio`);
+    noteAudio[0].load();
     noteAudio[0].play();
   };
   // click function
   $('.note').click((event) => {
     const note = event.target.innerHTML;
-    console.log(note);
     playNote(note);
   });
 
   // key press function
   $('body').keydown((event) => {
     const note = event.key;
-    console.log(note);
     playNote(note);
   });
 });
