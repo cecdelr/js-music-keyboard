5fd62935f5110599a30826b7c164e980b53dba01
Isaac Del Rosario <cecdelr@gmail.com>
2017-11-20 14:15:56 -0800

    functionality to play notes via mouse click

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
-$(document).ready( function() {
+$(document).ready(() => {
   // your code here
+  const NOTES = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+
+  const playNote = function playNote(note) {
+    const noteAudio = $(`#${note.toLowerCase()}Audio`);
+    noteAudio[0].play();
+  };
+  // click function
+  $('.note').click((event) => {
+    const note = event.target.innerHTML;
+    console.log(note);
+    playNote(note);
+  });
 });
